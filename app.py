9da39ec8571bfb9da708b228be5d104ab0522ae1
Yusuf Arif <yusufarifrahman20@gmail.com>
2025-04-22 09:20:05 +0700

    halah nyoco

--- app.py
+++ app.py
@@ -20,4 +20,7 @@ def chat():
     return jsonify({"reply": chatbot_reply})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
